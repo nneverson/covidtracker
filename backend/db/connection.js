780af876bdd750500aec8657b9a7756fbe16445b
nneverson <neelyneverson@yahoo.com>
2021-02-11 12:18:33 -0500

    add key to env file

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
-
-const mongoURI =
-	process.env.NODE_ENV === 'production'
-		? process.env.DB_URL
-		: 'mongodb://localhost/employees';
-
+require('dotenv').config();
+const mongoURI = process.env.MONGODB_URI;
 mongoose
 	.connect(mongoURI, {
 		useNewUrlParser: true,
